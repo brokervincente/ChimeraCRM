6b6c4de7f15ffd8fd508944c8f761fb72c062d3f
Broker Vincente <info@brokervincente.com>
2025-09-27 19:18:59 +0200

    Update agency.ts

--- src/types/agency.ts
+++ src/types/agency.ts
@@ -1,6 +1,6 @@
 export type Agency = {
   title: string;
   contacts: string;
-  logoUrl?: string | null; // opzionale, accetta string o null o undefined
+  logoUrl?: string | null;  // opzionale, accetta string, null e undefined
   logoCircular: boolean;
 };
