8dbeec51bced8d108ddb1c78943555c0980436d1
Broker Vincente <info@brokervincente.com>
2025-09-27 18:28:44 +0200

    Add files via upload

---
+++ src/pages/SettingsProfile.d.ts
@@ -0,0 +1,5 @@
+declare module './SettingsProfile' {
+  import React from 'react';
+  const SettingsProfile: React.FC<{agencyData: any; onSave: (data: any) => void}>;
+  export default SettingsProfile;
+}
