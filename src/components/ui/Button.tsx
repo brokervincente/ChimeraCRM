0363cbec7b029b1e54afa6d52c1168100dc56295
Broker Vincente <info@brokervincente.com>
2025-09-27 18:16:19 +0200

    Update Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,12 @@
-import React from react;
+import React from 'react';
 
-type Props = React.ButtonHTMLAttributesHTMLButtonElement;
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ className = , ...rest } Props) {
+export default function Button({ className = '', ...rest }: Props) {
   return (
-    button
-      className={`px-4 py-2 rounded-xl bg-blue-600 text-white hoverbg-blue-700 transition-shadow shadow ${className}`}
+    <button
+      className={`px-4 py-2 rounded-xl bg-blue-600 text-white hover:bg-blue-700 transition-shadow shadow ${className}`}
       {...rest}
-    
+    />
   );
 }
