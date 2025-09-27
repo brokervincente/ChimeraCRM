8b6ebeaf2acd2c48dc65ca315ed184774b4c25ac
Broker Vincente <info@brokervincente.com>
2025-09-27 18:30:49 +0200

    Update SettingsProfile.d.ts

--- src/pages/SettingsProfile.d.ts
+++ src/pages/SettingsProfile.d.ts
@@ -1,5 +1,5 @@
-declare module './SettingsProfile' {
-  import React from 'react';
-  const SettingsProfile: React.FC<{agencyData: any; onSave: (data: any) => void}>;
-  export default SettingsProfile;
-}
+import React from 'react';
+
+declare const SettingsProfile: React.FC<{ agencyData: any; onSave: (data: any) => void }>;
+
+export default SettingsProfile;
